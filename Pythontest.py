927991e7a9c329526d092c91d4809dc7432f1985
cn_wztz <ihuoyue@126.com>
2019-06-26 17:55:07 +0800

    Initial commit

---
+++ Pythontest.py
@@ -0,0 +1,25 @@
+import pymongo
+print("hi")
+print("Ctrl+D 复制当前行")
+print("Ctrl+Y 删除当前行")
+print("Ctrl+F 查找")
+print("Ctrl+R 替换")
+print("Ctrl+W 向'上'选择")
+print("Ctrl+/ 批量注释/取消注释")
+print("Ctrl+ '- +' 打开关闭代码块")
+print("Ctrl+鼠标左键 进入函数定义")
+print("tab 批量缩进")
+print("shift+tab 批量取消缩进")
+print("shift+回车 快速换行")
+print("shift+F10 运行 右上角选中的文件")
+print("ctrl+shift+F10 运行当前的文件")
+
+
+111 222 333 444
+111 222 333 444
+111 222 333 444
+111 222 333 444
+111 222 333 444
+111 222 333 444
+111 222 333 444
+
